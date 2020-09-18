84a40852b728c7b5ec5bf77977c7d91404a400a0
eboshare <t.lila@list.ru>
2020-09-19 02:50:05 +0400

    Add a navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_starter_template/injection/injection.dart';
 
 import 'package:flutter_starter_template/presentation/feature/counter/counter_page.dart';
+import 'package:flutter_starter_template/presentation/navigation/router.gr.dart';
 
 void main() {
   configureDependencies();
@@ -12,12 +14,14 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Starter Template',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: CounterPage(),
+      builder: ExtendedNavigator.builder(
+        router: Router(),
+      ),
     );
   }
 }
