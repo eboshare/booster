d5ff5a5999b7593be3c569e8e1d126787ab2ab4d
eboshare <t.lila@list.ru>
2021-01-02 02:24:15 +0300

    refactor: improve the file structure

--- lib/infrastructure/gallery/gallery.dart
+++ lib/infrastructure/gallery/gallery.dart
@@ -1,5 +1,5 @@
 export 'gallery_state_dto/gallery_state_dto.dart';
-export 'gallery_state_dto/gallery_state_converter_mixin.dart';
+export 'gallery_state_converter_mixin.dart';
 export 'image_local_dto/image_local_dto.dart';
 export 'image_network_dto/image_network_dto.dart';
 export 'image_repository/mock_image_repository.dart';
