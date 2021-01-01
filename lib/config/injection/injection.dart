ffbd3ebcc2f07432ac0a7b0aad1db3518e4bf3c4
eboshare <t.lila@list.ru>
2021-01-01 23:18:31 +0300

    refactor(injection): add logger to register module

--- lib/config/injection/injection.dart
+++ lib/config/injection/injection.dart
@@ -1,9 +1,8 @@
 import 'package:get_it/get_it.dart';
 import 'package:injectable/injectable.dart';
 import 'package:dio/dio.dart';
+import 'package:logger/logger.dart';
 import 'package:pretty_dio_logger/pretty_dio_logger.dart';
-
-import 'package:booster/config/logger.dart';
 
 import 'injection.config.dart';
 
@@ -14,6 +13,15 @@ void configureDependencies(String environment) => $initGetIt(getIt, environment:
 
 @module
 abstract class RegisterModule {
+  @lazySingleton
+  Logger get logger {
+    return Logger(
+      printer: SimplePrinter(
+        colors: false,
+      ),
+    );
+  }
+
   @singleton
   Dio get dio {
     return Dio()
