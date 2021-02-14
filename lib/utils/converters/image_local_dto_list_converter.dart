97b0244103efc187815478bcb0f5d731a9bd68a9
eboshare <t.lila@list.ru>
2021-02-14 11:21:12 +0300

    fix: change image local dto source type in converter to fix the bug when serializing

--- lib/utils/converters/image_local_dto_list_converter.dart
+++ lib/utils/converters/image_local_dto_list_converter.dart
@@ -1,9 +1,9 @@
 import 'package:booster/infrastructure/gallery/image_local_dto/image_local_dto.dart';
 import 'package:booster/utils/converters/kt/kt_list_converter.dart';
 
-ImageLocalDto _objectFromJson(Map<String, dynamic> json) => ImageLocalDto.fromJson(json);
+ImageLocalDto _objectFromJson(Object json) => ImageLocalDto.fromJson(json);
 Map<String, dynamic> _objectToJson(ImageLocalDto dto) => dto.toJson();
 
-class ImageLocalDtoListConverter extends KtListConverter<ImageLocalDto, Map<String, dynamic>> {
+class ImageLocalDtoListConverter extends KtListConverter<ImageLocalDto, Object> {
   const ImageLocalDtoListConverter() : super(_objectFromJson, _objectToJson);
 }
