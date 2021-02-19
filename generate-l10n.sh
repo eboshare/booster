79f5701c6423cc1548c06c7d3a685337177b8b39
eboshare <t.lila@list.ru>
2021-02-19 15:52:35 +0300

    feat: small improvement

--- generate-l10n.sh
+++ generate-l10n.sh
@@ -5,7 +5,7 @@ readonly PACKAGE_LIST=$(flutter pub global list)
 readonly GREP_OUTPUT=$(echo "$PACKAGE_LIST" | grep "$INTL_UTILS_PACKAGE")
 
 if [ "$GREP_OUTPUT" == "" ]; then
-  echo "Activation of intl_utils"
+  echo "Activation of $INTL_UTILS_PACKAGE"
   flutter pub global activate "$INTL_UTILS_PACKAGE"
 fi
 
